c757dc2a594557904496dc65895c5aa9e5366d2b
tdihp <tdihp@hotmail.com>
2012-09-05 07:46:36 +0800

    fill codes for table

--- coldb/table.py
+++ coldb/table.py
@@ -3,6 +3,7 @@ Created on Sep 1, 2012
 
 @author: pp
 '''
+from .common import col_uniname
 
 
 def _cmprow(idxarr, row1, row2):
@@ -14,9 +15,17 @@ def _cmprow(idxarr, row1, row2):
 
 
 class Table(object):
-    def __init__(self, schema, idx, colnames, pkey=None, skeys=None):
+    def __init__(self, schema, idx, col_uninames, pkey=None, skeys=None):
         self._schema = schema
         self._idx = idx
+
+
+        self._cols = list((schema.col_by_uniname[col_uniname].idx, col_uniname, schema.col_by_uniname[col_uniname]) for col_uniname in col_uninames)
+        cols = []
+        for col_uniname in col_uninames:
+            col = schema.col_by_uniname[col_uniname]
+            cols.append(col)
+        cols = sorted(cols, key=col.idx)
 
     @property
     def schema(self):
@@ -38,8 +47,12 @@ class Table(object):
     def idx(self):
         pass
 
+    def feed_rows(self, rows):
+        pass
+
     def catch_fkey(self):
         pass
 
-    def sortrows(self):
+    def sort_rows(self, rows):
+#        sort_idxarr = list(for row in rows)
         pass
