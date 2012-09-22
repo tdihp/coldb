67dee8938a11e8d3c98547f7b5db7aa9ebacf29a
tdihp <tdihp@hotmail.com>
2012-09-22 19:44:41 +0800

    basic implementations for interger arrs all done, yeah!

--- cpp/include/column.hpp
+++ cpp/include/column.hpp
@@ -6,6 +6,25 @@
 
 namespace coldb{
 
+template <typename T>
+inline void* aligned(void* ptr)
+{
+  U32 align_mask = sizeof(T)-1;
+  U32 u_ptr = (U32)ptr;
+  U32 tail = u_ptr & align_mask;
+  if(tail)
+  {
+    return (void*) (u_ptr + sizeof(T) - tail);
+  }
+  return ptr;
+}
+
+template <>
+inline void* aligned<U8>(void* ptr) {return ptr;}
+
+template <>
+inline void* aligned<I8>(void* ptr) {return ptr;}
+
 // abstract interface of all column objects
 // IFType: type of interface
 // ACType: type of actual storage
@@ -128,8 +147,117 @@ struct PlainImpl
 
 
 template <typename DT, typename PT>
-struct Run0Impl{
+struct Run0Impl
+{
+  I32 data_size_;
+  I32 run_cnt_;
+  PT* run_ptr_;
+  DT* data_ptr_;
+  
+  Run0Impl(void* data_ptr, I32 data_size) : data_size_(data_size)
+  {
+    run_cnt_ = *((PT*)data_ptr);
+    run_ptr_ = ((PT*)data_ptr) + 1;
+    data_ptr_ = aligned<DT>(run_ptr_ + run_cnt_);
+  }
+  
+  DT get(I32 rowid)
+  {
+    // find rowid in run_ptr
+    I32 runid = std::lower_bound(run_ptr_, run_ptr_ + run_cnt_, rowid);
+    
+    if(run_ptr_[runid] != rowid)
+    {
+      --runid;
+    }
+    // rtn data from data_ptr
+    return data_ptr_[runid];
+  }
+  
+  I32 find(DT var)
+  {
+    I32 runid = std::lower_bound(data_ptr_, data_ptr_ + run_cnt_, var);
+    if(data_ptr_[runid] == var)
+    {
+      return run_ptr_[runid];
+    }
+    return -1;
+  }
 };
+
+template <typename DT, typename PT>
+struct Run1Impl : public Run0Impl<DT, PT>
+{
+  DT get(I32 rowid)
+  {
+    // find rowid in run_ptr
+    I32 runid = std::lower_bound(run_ptr_, run_ptr_ + run_cnt_, rowid);
+    
+    if(run_ptr_[runid] != rowid)
+    {
+      --runid;
+    }
+    return data_ptr_[runid] + (rowid - run_ptr_[runid]);
+  }
+  
+  I32 find(DT var)
+  {
+    I32 runid = std::lower_bound(data_ptr_, data_ptr_ + run_cnt_, var);
+    DT* this_run = run_ptr_ + runid;
+    I32 runlen;
+    I32 diff = var - data_ptr_[runid];
+    // get length of this run
+    if(runid == (run_cnt_ - 1))
+    {
+      runlen = data_size_ - *this_run;
+    }
+    else
+    {
+      runlen = *(this_run + 1) - *this_run;
+    }
+    
+    if(diff < runlen)
+    {
+      return *this_run + diff;
+    }
+    return -1;
+  }
+};
+
+template <typename DT, typename ET>
+struct EnumImpl
+{
+  I32 data_size_;
+  I32 enum_cnt_;
+  ET* new_val_ptr_;
+  DT* enum_ptr_;
+  EnumImpl(void* data_ptr, I32 data_size) : data_size_(data_size)
+  {
+    enum_cnt_ = *((ET*)data_ptr);
+    new_val_ptr_ = ((ET*)data_ptr) + 1;
+    enum_ptr_ = aligned<DT>(new_val_ptr_ + data_size_);
+  }
+  
+  DT get(I32 rowid)
+  {
+    return enum_ptr_[new_val_ptr_[rowid]];
+  }
+  
+  // find is oddly used, whatever
+  I32 find(DT var)
+  {
+    enum_i = std::lower_bound(enum_ptr_, enum_ptr_ + enum_cnt_, var);
+    if(enum_ptr_[enum_i] != var)
+    {
+      return -1;
+    }
+    
+    return std::lower_bound(new_val_ptr_, new_val_ptr_ + data_size_, enum_i);
+  }
+}
+
+
+
 
 }
 #endif
